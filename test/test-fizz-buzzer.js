369b0deeadf80831aeb1ed696704d23403279095
Kyle Tozer <kwtozer@gmail.com>
2018-02-24 17:13:02 -0500

    first commit

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const fizzbuzz = require('../fizzBuzzer');
+
+describe('fizzbuzz', function() {
+
+  it('should return a number or string depending on the number passed to it', function() {
+    expect(fizzbuzz(1)).to.equal(1);
+    expect(fizzbuzz(3)).to.equal('fizz');
+    expect(fizzbuzz(5)).to.equal('buzz');
+    expect(fizzbuzz(15)).to.equal('fizz-buzz');
+  });
+
+  it('should throw an error if passed an input other than a number', function() {
+    expect(fizzbuzz(true)).to.throw();
+    expect(fizzbuzz({})).to.throw();
+    expect(fizzbuzz()).to.throw();
+  });
+});
